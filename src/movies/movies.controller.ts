9c917d943e50831f7a01cb1473be89adb5306761
youngkwon02 <youngkwon02@naver.com>
2022-04-16 17:05:02 +0900

    feat: use @Param at controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller('movies')
-export class MoviesController {}
+@Controller('movies') // Entry Point
+export class MoviesController {
+  @Get()
+  getAll(): string {
+    return 'This will return all movies!';
+  }
+
+  @Get('/:id')
+  getOneMovie(@Param('id') movieId: string): string {
+    return `This will return a movie whose Id is ${movieId}`;
+  }
+}
