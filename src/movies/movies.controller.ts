a6e3e1ea3f1d2a0da55a798e5b2f6aaea91be040
youngkwon02 <youngkwon02@naver.com>
2022-04-17 14:34:38 +0900

    refacotr: updateData with DTO

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -32,7 +33,7 @@ export class MoviesController {
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
