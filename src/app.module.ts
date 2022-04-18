398871faa273e3da26c1a113ecbbc81c39b7ab57
youngkwon02 <youngkwon02@naver.com>
2022-04-18 16:01:51 +0900

    chore: seperate controller and provider from the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [MoviesModule],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
